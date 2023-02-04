ac5d645486d12f08d967291f05d919c757260562
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-04 17:58:30 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "25315e39-fb13-446b-979b-e4eb7730e881",
+    "home_feed": [
+        {
+            "id": "05b6a691-9efe-47ff-b156-2c6928a646b2",
+            "artist_name": "truth",
+            "home_carousel": [
+                {
+                    "id": "2a2491bb-9778-4e2b-aaaf-58c0495894ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "a8cf6aa5-fe2d-47db-b015-ce01a4f89a7d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "90408f78-aa11-459a-93c7-0be306278aa7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "306d9ed0-7ba6-4ef0-ac26-64522d152df8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "ace44f7c-ebaf-4b64-a7e9-49acbb5c2b29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/61.png"
+                }
+            ],
+            "art_caption": "kitchen",
+            "art_description": "Forget nature agency certain cut. Price sometimes left leave break among development move."
+        },
+        {
+            "id": "61c9fe37-ea6a-413c-be0f-87135d6df9cc",
+            "artist_name": "fear",
+            "home_carousel": [
+                {
+                    "id": "46309a69-a051-4aca-8721-a2a95f801ac8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "90e3eb05-f2c2-433a-9c5f-e0a33374f477",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/294.png"
+                },
+                {
+                    "id": "3bed55c5-35df-46c3-b333-35646ff4ecdb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "bd9d84db-106e-4a92-8434-a7ef2e217164",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/63.png"
+                },
+                {
+                    "id": "0a4318c5-169b-43b3-b722-14559756f724",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/322.png"
+                }
+            ],
+            "art_caption": "scientist",
+            "art_description": "Able front rest about child likely recognize. Military special change themselves. Color economic moment research."
+        },
+        {
+            "id": "efb5cbeb-63cc-46c1-8da3-459e27cadf62",
+            "artist_name": "of",
+            "home_carousel": [
+                {
+                    "id": "9fcd3925-a71c-488b-b54e-c51a322292b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                },
+                {
+                    "id": "e7b817b5-f36e-4ded-a7b5-fdc1e3799058",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                },
+                {
+                    "id": "1672d8f3-df93-4d67-bd1e-ea2cfa261586",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/33.png"
+                },
+                {
+                    "id": "880f0538-9051-4f8d-a676-c912107acb4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "f5dfbeda-27f6-4981-885a-a1647b755104",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png"
+                }
+            ],
+            "art_caption": "throw",
+            "art_description": "Those situation right care push yourself somebody. Would spend draw item ahead side win. Season level you up that analysis her officer. Occur economy news Mrs sound happy risk."
+        },
+        {
+            "id": "68dc3a32-e8ee-408d-96cc-29eb467b6030",
+            "artist_name": "option",
+            "home_carousel": [
+                {
+                    "id": "c6fc574b-9478-4fb0-8aae-d93b9d1a6dd3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/214.png"
+                },
+                {
+                    "id": "712fa1b6-e3fc-494d-83de-fb9e0027a783",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "7aaa8278-f754-453b-85c9-9017fdb6b34b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png"
+                },
+                {
+                    "id": "13a49978-7450-4add-89e6-e1e1ac60a3dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "5daa0106-cacd-436a-a465-1ec26dbed748",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                }
+            ],
+            "art_caption": "quickly",
+            "art_description": "May include its nor name."
+        },
+        {
+            "id": "9f57e9a8-2f40-423f-8807-9ff1258642a7",
+            "artist_name": "garden",
+            "home_carousel": [
+                {
+                    "id": "8678a365-be00-4793-a79f-dc1f90d737dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "80acb3a1-2e5e-4273-b8df-b05b16012fe7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+                },
+                {
+                    "id": "985abeef-ebce-4147-a2c3-dd74ba63a041",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "0351ef5a-ae1a-4a93-8d12-2474c904eeb7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "baf30d5f-de81-4da1-9db8-9b3d6269cfee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/53.png"
+                }
+            ],
+            "art_caption": "pattern",
+            "art_description": "Free realize stop ever. Manage note line born well fast argue space."
+        }
+    ]
+}
